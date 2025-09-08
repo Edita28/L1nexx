570d155602bd2785035e1bbcf034f1e6f06a52cd
L1nexx <ellkaellka1337@gmail.com>
2025-09-08 18:59:13 +0300

    Adaptive About

--- src/components/pages/about/cover/cover.tsx
+++ src/components/pages/about/cover/cover.tsx
@@ -6,6 +6,12 @@ const defaultCoverData: CoverProps = {
     CoverHeader: {
         header: 'Обо мне',
         header_info: 'Узнайте больше о моем пути в разработке'
+    },
+    CoverInfo: {
+        heading: 'Мой путь в программировании',
+        CoverInfo__text_1: "Привет! Меня зовут Stokeyer, и я начинающий Frontend\nразработчик с большой страстью к созданию красивых и\nфункциональных веб-интерфейсов. Мой путь в программировании\nначался с изучения основ  HTML и CSS, и с тех пор\nя постоянно развиваюсь в этой области.",
+        CoverInfo__text_2: "За время обучения я освоил современные технологии, такие\nкак React, TypeScript, и различные инструменты для стилизации\nвключая SCSS и CSS-фреймворки. Я верю в важность написания\nчистого, поддерживаемого кода и следую лучшим практикам\nразработки.",
+        CoverInfo__text_3: "Моя цель — стать частью команды, где я смогу применить свои\nзнания, продолжить обучение и внести вклад в создание\nкачественных продуктов, которые решают реальные проблемы\nпользователей."
     }
 }
 
@@ -14,6 +20,7 @@ const defaultCoverData: CoverProps = {
 export const Cover: React.FC<CoverProps> = (props = defaultCoverData) => {
     const CoverData = { ...defaultCoverData, ...props };
     const CoverHeader= CoverData.CoverHeader || defaultCoverData.CoverHeader;
+    const CoverInfo= CoverData.CoverInfo || defaultCoverData.CoverInfo;
 
     return(
         <>
@@ -23,9 +30,19 @@ export const Cover: React.FC<CoverProps> = (props = defaultCoverData) => {
                 <p>{CoverHeader?.header_info}</p>
                 <section className={cl.Cover__section}>
                     <div className={cl.Cover__information}>
+                        <h2>{CoverInfo?.heading}</h2>
+                        <div className={cl.Cover__information_container}>
+                            <p>{CoverInfo?.CoverInfo__text_1}</p>
+                            <p>{CoverInfo?.CoverInfo__text_2}</p>
+                            <p>{CoverInfo?.CoverInfo__text_3}</p>
+                        </div>
                         
                     </div>
-                    <div className={cl.Cover__cards}></div>
+                    <div className={cl.Cover__cards}>
+                        <div className={cl.Cover__cards_facts}>
+                            
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
--- src/components/pages/about/cover/types.ts
+++ src/components/pages/about/cover/types.ts
@@ -1,9 +1,17 @@
 export interface CoverProps{
     className?: string
     CoverHeader?: CoverHeader;
+    CoverInfo?: CoverInfo;
 }
 
 export interface CoverHeader{
     header: string;
     header_info: string;
+}
+
+export interface CoverInfo{
+    heading: string;
+    CoverInfo__text_1: string;
+    CoverInfo__text_2: string;
+    CoverInfo__text_3: string;
 }
