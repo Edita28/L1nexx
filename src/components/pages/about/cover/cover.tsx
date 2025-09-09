ba5ae79e6c48ca5cba60c28f64d40f025339c52c
L1nexx <ellkaellka1337@gmail.com>
2025-09-09 20:45:57 +0300

    Adaptive About#4

--- src/components/pages/about/cover/cover.tsx
+++ src/components/pages/about/cover/cover.tsx
@@ -12,6 +12,17 @@ const defaultCoverData: CoverProps = {
         CoverInfo__text_1: "Привет! Меня зовут Stokeyer, и я начинающий Frontend\nразработчик с большой страстью к созданию красивых и\nфункциональных веб-интерфейсов. Мой путь в программировании\nначался с изучения основ  HTML и CSS, и с тех пор\nя постоянно развиваюсь в этой области.",
         CoverInfo__text_2: "За время обучения я освоил современные технологии, такие\nкак React, TypeScript, и различные инструменты для стилизации\nвключая SCSS и CSS-фреймворки. Я верю в важность написания\nчистого, поддерживаемого кода и следую лучшим практикам\nразработки.",
         CoverInfo__text_3: "Моя цель — стать частью команды, где я смогу применить свои\nзнания, продолжить обучение и внести вклад в создание\nкачественных продуктов, которые решают реальные проблемы\nпользователей."
+    },
+    CoverCards: {
+        header: 'Быстрые факты',
+        location: 'Локация',
+        locationInfo: 'Симферополь, Россия',
+        experience: 'Опыт',
+        experienceInfo: 'Начинающий\nразработчик',
+        education: 'Образование',
+        educationInfo: 'Самообучение + курсы',
+        language: 'Языки',
+        languageInfo: 'Русский (родной),\nАнглийский (A1)'
     }
 }
 
@@ -19,8 +30,9 @@ const defaultCoverData: CoverProps = {
 
 export const Cover: React.FC<CoverProps> = (props = defaultCoverData) => {
     const CoverData = { ...defaultCoverData, ...props };
-    const CoverHeader= CoverData.CoverHeader || defaultCoverData.CoverHeader;
-    const CoverInfo= CoverData.CoverInfo || defaultCoverData.CoverInfo;
+    const CoverHeader = CoverData.CoverHeader || defaultCoverData.CoverHeader;
+    const CoverInfo = CoverData.CoverInfo || defaultCoverData.CoverInfo;
+    const CoverCards = CoverData.CoverCards  || defaultCoverData.CoverCards
 
     return(
         <>
@@ -35,12 +47,27 @@ export const Cover: React.FC<CoverProps> = (props = defaultCoverData) => {
                             <p>{CoverInfo?.CoverInfo__text_1}</p>
                             <p>{CoverInfo?.CoverInfo__text_2}</p>
                             <p>{CoverInfo?.CoverInfo__text_3}</p>
-                        </div>
-                        
+                        </div>  
                     </div>
                     <div className={cl.Cover__cards}>
                         <div className={cl.Cover__cards_facts}>
-                            
+                            <h3>{CoverCards?.header}</h3>
+                            <section>
+                                <h4>{CoverCards?.location}</h4>
+                                <p>{CoverCards?.locationInfo}</p>
+                            </section>
+                            <section>
+                                <h4>{CoverCards?.experience}</h4>
+                                <p>{CoverCards?.experienceInfo}</p>
+                            </section>
+                            <section>
+                                <h4>{CoverCards?.education}</h4>
+                                <p>{CoverCards?.educationInfo}</p>
+                            </section>
+                            <section>
+                                <h4>{CoverCards?.language}</h4>
+                                <p>{CoverCards?.languageInfo}</p>
+                            </section>
                         </div>
                     </div>
                 </section>
--- src/components/pages/about/cover/types.ts
+++ src/components/pages/about/cover/types.ts
@@ -2,6 +2,7 @@ export interface CoverProps{
     className?: string
     CoverHeader?: CoverHeader;
     CoverInfo?: CoverInfo;
+    CoverCards?: CoverCards;
 }
 
 export interface CoverHeader{
@@ -14,4 +15,16 @@ export interface CoverInfo{
     CoverInfo__text_1: string;
     CoverInfo__text_2: string;
     CoverInfo__text_3: string;
+}
+
+export interface CoverCards{
+    header: string;
+    location: string;
+    locationInfo: string;
+    experience: string;
+    experienceInfo: string;
+    education: string;
+    educationInfo: string
+    language: string;
+    languageInfo: string;
 }
